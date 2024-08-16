5973e3aea5d76a209c640add50961b7a522a08cf
AwesomeSapir <melchinsapir@gmail.com>
2024-08-16 15:02:55 +0300

    added retrofit library and singleton RetrofitClient

---
+++ app/src/main/java/com/sapreme/reqresclient/data/RetrofitClient.java
@@ -0,0 +1,22 @@
+package com.sapreme.reqresclient.data;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static final String BASE_URL = "https://reqres.in/api/";
+    private static Retrofit retrofit;
+
+    private RetrofitClient(){}
+
+    public static Retrofit getInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
